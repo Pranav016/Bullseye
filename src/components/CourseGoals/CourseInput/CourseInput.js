203a745109840ac67cada07821f9e115fd114d0c
Pranav <pranavmendi@gmail.com>
2021-04-24 11:29:49 +0530

    Dynamically adding style to the component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,27 +3,35 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+	const [enteredValue, setEnteredValue] = useState('');
+	const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+	const goalInputChangeHandler = (event) => {
+		setEnteredValue(event.target.value);
+		if (enteredValue.trim().length === 0) {
+			setIsValid(true);
+		}
+	};
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			setIsValid(false);
+			return; // we need to end function execution after this hence we return
+		}
+		props.onAddGoal(enteredValue);
+	};
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className={`form-control ${isValid ? '' : 'invalid'}`}>
+				<label>Course Goal</label>
+				<input type='text' onChange={goalInputChangeHandler} />
+			</div>
+			<Button type='submit'>Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
